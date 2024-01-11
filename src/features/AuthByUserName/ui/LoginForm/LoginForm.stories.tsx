603beedb6d8556b2b22a80a65ba3d6fef05efbdf
Vayt-Vayt <vayt08979@gmail.com>
2024-01-11 12:46:19 +0400

    add stories logiform and loginmodal

---
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import "app/styles/index.scss"
+import { ThemeDecorators } from 'shared/config/storybook/decorators/ThemeDecorators';
+import { Theme } from 'app/provider/themeProvider';
+import { LoginForm } from './LoginForm';
+
+
+const meta = {
+    title: 'features/LoginForm',
+    component: LoginForm,
+    parameters: {},
+    argTypes: {},
+    args: {
+        
+    }
+
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+    args: {},
+
+};
+
+export const Dark: Story = {
+    args: {},
+    decorators: [
+        (Story) => ThemeDecorators(Theme.DARK)(Story)
+    ]
+
+};
---
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import "app/styles/index.scss"
+import { ThemeDecorators } from 'shared/config/storybook/decorators/ThemeDecorators';
+import { Theme } from 'app/provider/themeProvider';
+import { LoginModal } from './LoginModal';
+
+
+const meta = {
+    title: 'features/LoginModal',
+    component: LoginModal,
+    parameters: {},
+    argTypes: {},
+    args: {
+        isOpen: true,
+    }
+
+} satisfies Meta<typeof LoginModal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+    args: {},
+
+};
+
+export const Dark: Story = {
+    args: {},
+    decorators: [
+        (Story) => ThemeDecorators(Theme.DARK)(Story)
+    ]
+
+};
